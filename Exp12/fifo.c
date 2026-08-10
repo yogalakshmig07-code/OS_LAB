d1648edb0f480819b54cc9e275a7411b72c6f80f
yogalakshmig07-code <yogalakshmig07@gmail.com>
2026-08-10 09:18:46 -0400

    Added Experiment 12

---
+++ Exp12/fifo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int pages[50],frames[10],n,f,i,j,k=0,fault=0,found;
+printf("Enter Number of Pages: ");
+scanf("%d",&n);
+printf("Enter Reference String:\n");
+for(i=0;i<n;i++) scanf("%d",&pages[i]);
+printf("Enter Number of Frames: ");
+scanf("%d",&f);
+for(i=0;i<f;i++) frames[i]=-1;
+for(i=0;i<n;i++){
+found=0;
+for(j=0;j<f;j++) if(frames[j]==pages[i]){found=1;break;}
+if(!found){frames[k]=pages[i];k=(k+1)%f;fault++;}
+}
+printf("Total Page Faults = %d\n",fault);
+return 0;
+}
