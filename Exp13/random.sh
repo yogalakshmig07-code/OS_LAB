1aa9892a9c30d733fc13b46eb7d7a47a808ebd3d
yogalakshmig07-code <yogalakshmig07@gmail.com>
2026-08-19 09:00:22 -0400

    Added Experiment 13 file organization techniques

---
+++ Exp13/random.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter Record:"
+read rec
+
+echo "$rec" > random.txt
+
+echo "Random Access Record"
+
+sed -n '1p' random.txt
+
---
+++ Exp13/sequential.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter Register Number:"
+read regno
+
+echo "Enter Name:"
+read name
+
+echo "$regno $name" > student.txt
+
+echo "Contents of File"
+
+cat student.txt
+
